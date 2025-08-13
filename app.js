c9c403fb5484c1e97cf9ed497d6410acb84d4576
MARCOS RYAN SILVA DAS NEVES <25132758@CFP127ALUNO.LOCAL>
2025-08-13 10:09:11 -0300

    Código comentado e finalizado

--- app.js
+++ app.js
@@ -3,36 +3,40 @@
 // Importa os dados do arquivo JSON.
 import funcionarios from "./funcionarios.json" with {type: "json"}
 
+//Adicionando o body
 const body = document.querySelector('body')
 body.classList.add('body') 
 
+//Adicionando a classe container
 const container = document.querySelector('.container')
 
+//Leitura de array
 funcionarios.forEach((funcionarios) => {
 
+    //Cria a div com os cards
     const card = document.createElement('div')
     card.classList.add('card')
-
-
 
     // Cria e configura a imagem
     const imagem = document.createElement('img')
     imagem.src = funcionarios.imagem
     imagem.alt = `Foto de ${funcionarios.nome}`
 
-    // Cria o nome
+    // Cria o nome do funcionário
     const nome = document.createElement('span')
     nome.textContent = funcionarios.nome
 
-    //Cria o cargo
+    //Cria o cargo do funcionário
     const cargo = document.createElement('p')
     cargo.textContent = funcionarios.cargo
 
     // Adiciona todos os elementos criados dentro do card
     card.append(imagem, nome, cargo)
 
+    //Adiciona o card ao container
     container.appendChild(card)
 
+    //Adiciona o container ao body
     body.appendChild(container)
 
 })
